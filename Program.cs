85d607639924154836b8c06b49ab47f4f9d383e2
zbgk894-design <zbgk894@gmail.com>
2026-06-16 04:22:37 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UntieUnite.Core;
+
+namespace UntieUnite
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                Console.WriteLine("usage: in_dir out_dir");
+                Console.WriteLine(@"in_dir: full-path with downloaded assets (not apk), aka *\assets\DlcRoot\0.3.0\DLC_0");
+                Console.WriteLine("out_dir: full-path to save all dumped data to.");
+                return;
+            }
+
+            var inDir = args[0];
+            var outDir = args[1];
+            Dumper.ExtractBins(inDir, outDir);
+        }
+    }
+}
